32a9ad2a7b30450d8c6d26023901efba9693f855
W1sCont <tmrostik@gmail.com>
2025-10-15 20:41:40 +0200

    v1.1 поле і розположення мін

--- miner.py
+++ miner.py
@@ -1,36 +1,96 @@
 import tkinter as tk
 import time
-
-# func 
-#розміщення кнопок на полі
-def add_bottom():
-    for row in range(row_in_win):
-        for col in range(column_in_win):
-            tk.Button(window, text=f"{col}, {row}", command= "").grid(row = row, column = col)
-
-
+from random import shuffle
 
 # ===
 # window
 
-win_width = 700
+win_width = 1200
 win_height = 600
 
 window = tk.Tk()
 window.title("Сапер")
 window.config(bg= "#e6f0ef")
-window.geometry(f"{win_width}x{win_height}+500+100")
+window.geometry(f"{win_width}x{win_height}+100+100")
 img_icon = tk.PhotoImage(file = "img_mine.png")
 window.iconphoto(False, img_icon)
-
 # ===
+
 # змінні для к-сті кнопок на полі
 column_in_win = 10
 row_in_win = 10
+mines = 5
+
+# func 
+
+
+#розміщення кнопок на полі
+def create_bottoms():
+    for el_row in range(row_in_win):
+        for el_col in range(column_in_win):
+            btn = buttons[el_row][el_col]
+            btn.grid(row=el_row, column=el_col)
+
+
+def start():
+    create_bottoms()
+    window.mainloop()
+
+
+# випадкові числа для мін в зрізі вказаному користувачем
+def random_index_for_mines():
+    btn_index = list(range(1, column_in_win * row_in_win + 1))
+    shuffle(btn_index)
+    return btn_index[:mines]
+        
+        
+# перетворити кнопку на бомбу
+def is_mine():
+    pass
+
+
+def main():
+    start()
+
+
+# розміщення бомб
+def insert_mines():
+    pass
 
 # ===
-add_bottom() # виклик функції для розміщення кнопок
-window.mainloop()
 
-# if __name__ == '__window__':  # перевірка, що файл запускається вручну
-#     window()
+
+
+# створення вкладеного списку для кнопок
+buttons = []
+count = 1
+num_of_btn = False
+index_mines = random_index_for_mines()
+print(index_mines)
+for el_row in range(row_in_win):
+    temp = []
+    for el_col in range(column_in_win):
+        if count in index_mines:
+            num_of_btn = True
+            btn = tk.Button(window,text=f"{el_row}, {el_col}, #{count}, {num_of_btn}" , width=10, font='sans-serif 12 bold', bg="red")
+            temp.append(btn)
+            count += 1
+        else:
+            num_of_btn = False
+            btn = tk.Button(window,text=f"{el_row}, {el_col}, #{count}, {num_of_btn}" , width=10, font='sans-serif 12 bold')
+            temp.append(btn)
+            count += 1
+    buttons.append(temp)
+
+# грід кнопок на полі def create_bottoms()
+# for el_row in range(row_in_win):
+#     for el_col in range(column_in_win):
+#         btn = buttons[el_row][el_col]
+#         btn.grid(row=el_row, column=el_col)
+
+
+# ===
+
+
+if __name__ == '__main__':
+    main()
