a6c4463f8578a657acc2710fd4afb3b19387053f
W1sCont <tmrostik@gmail.com>
2025-10-17 16:15:26 +0200

    v1.2.1 menu

--- miner.py
+++ miner.py
@@ -18,18 +18,7 @@ colors = {
 }
 
 
-# window
 
-win_width = 600
-win_height = 600
-
-window = tk.Tk()
-window.title("Сапер")
-window.config(bg= "#e6f0ef")
-window.geometry(f"{win_width}x{win_height}+100+100")
-img_icon = tk.PhotoImage(file = "img_mine.png")
-window.iconphoto(False, img_icon)
-# ===
 
 # змінні для к-сті кнопок на полі
 column_in_win = 10
@@ -38,7 +27,6 @@ mines = 25
 buttons = {}
 
 # func 
-
 
 #розміщення кнопок на полі
 def create_bottoms():
@@ -55,6 +43,7 @@ def random_mines_coords():
     shuffle(btn_index)
     return set(btn_index[:mines])
 
+
 # присвоєння змінній рандомного списку
 random_mines = random_mines_coords()
 
@@ -68,6 +57,7 @@ def click(row, col):
         number = count_adjacent(row, col)
         color = colors.get(number, "black")
         btn.config(text=number, bg="lightgray", state="disabled", disabledforeground=color)
+
 
 def game_over():
     for (row_el, col_el), btn in buttons.items():
@@ -85,13 +75,79 @@ def count_adjacent(row, col):
     return count
 
 
+def reload():
+    global random_mines, buttons
+    for child in window.winfo_children():
+        child.destroy()
+    buttons.clear()
+    
+    create_menu()
+    random_mines = random_mines_coords()
+    create_bottoms()
+
+
+def settings_win():
+    win_settings = tk.Toplevel(window)
+    win_settings.title("Налаштування")
+
+    tk.Label(win_settings, text="К-сть рядків: "). grid(row=0, column=0, padx=20, pady=20)
+    row_in_win_entry = tk.Entry(win_settings)
+    row_in_win_entry.grid(row=1, column=1, padx=20, pady=20)
+    row_in_win_entry.insert(0, str(row_in_win))
+
+    tk.Label(win_settings, text="К-сть стовпців: "). grid(row=1, column=0, padx=20, pady=20)
+    column_in_win_entry = tk.Entry(win_settings)
+    column_in_win_entry.grid(row=0, column=1, padx=20, pady=20)
+    column_in_win_entry.insert(0, str(column_in_win))
+
+    tk.Label(win_settings, text="К-сть мін: "). grid(row=2, column=0, padx=20, pady=20)
+    mines_entry = tk.Entry(win_settings)
+    mines_entry.grid(row=2, column=1, padx=20, pady=20)
+    mines_entry.insert(0, str(mines))
+
+    btn_ok = tk.Button(win_settings, text="ok", command=lambda: change_setting(row_in_win_entry, column_in_win_entry, mines_entry))
+    btn_ok.grid(row=3, column=0, columnspan=2)
+
+
+# try except перевірити чи не викливає помилку коли ввели не число
+def change_setting(row_in_win_entry, column_in_win_entry, mines_entry):
+    global row_in_win, column_in_win, mines, random_mines, buttons
+    row_in_win = int(row_in_win_entry.get())
+    column_in_win = int(column_in_win_entry.get())
+    mines = int(mines_entry.get())
+    reload()
+
+
+def create_menu():
+    menubar = tk.Menu(window)
+    window.config(menu=menubar)
+    settings_menu = tk.Menu(menubar, tearoff=0)
+    settings_menu.add_command(label="Game", command=reload)
+    settings_menu.add_command(label="Settings", command=settings_win)
+    settings_menu.add_command(label="Exit", command=window.destroy)
+    menubar.add_cascade(label="Menu", menu=settings_menu)
+
+
 def main():
+    create_menu()
     create_bottoms()
     window.mainloop()
 
 
 # ===
+# window
 
+# win_width = 600
+# win_height = 600
+
+window = tk.Tk()
+window.title("Сапер")
+window.config(bg= "#e6f0ef")
+# window.geometry(f"{win_width}x{win_height}+100+100")
+img_icon = tk.PhotoImage(file = "img_mine.png")
+window.iconphoto(False, img_icon)
+
+# window
 
 
 
